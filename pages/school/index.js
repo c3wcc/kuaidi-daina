9b502e3ced3bbc7321978f3843e0b91645a18dc0
管理员 <www@lang100.com>
2018-10-15 10:37:15 +0800

    更新

--- pages/school/index.js
+++ pages/school/index.js
@@ -1,3 +1,4 @@
+import Page from '../../common/page';
 const APP_ID = 'wx2c985a73e416f727';//输入小程序appid  
 const APP_SECRET = 'cad42e76b18a5854f201b7ebceb024f4';//输入小程序app_secret  
 var OPEN_ID = ''//储存获取到openid  
